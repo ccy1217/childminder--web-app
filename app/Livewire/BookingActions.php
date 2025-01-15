8e8a4c559dc555eb69b297ecba8307739526e168
ccy1217code <chungyan02@gmail.com>
2025-01-15 15:16:25 +0000

    first commit

---
+++ app/Livewire/BookingActions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class BookingActions extends Component
+{
+    public function render()
+    {
+        return view('livewire.booking-actions');
+    }
+}
